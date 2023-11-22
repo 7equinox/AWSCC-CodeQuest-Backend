c8ec0d5e5e7703399f5e3b4fe1154af6174c66eb
7equinox <juliusevanestonilo08@gmail.com>
2023-11-22 12:57:20 +0800

    Backend (Day 1/30)

---
+++ backend/day-01/challengeday1.py
@@ -0,0 +1,6 @@
+my_name = "Julius"
+name_of_department = "DSWD"
+about_me = "I love coding"
+
+print(f"""Hello, I am {my_name}. I love {name_of_department} and I love being in the Backend even more! \
+One thing about me is that {about_me}.""")
