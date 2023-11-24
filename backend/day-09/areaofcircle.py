9e7c041e921fbaab330595f50166665f58345adb
7equinox <juliusevanestonilo08@gmail.com>
2023-11-24 20:57:31 +0800

    Backend (Day 9/30)

---
+++ backend/day-09/areaofcircle.py
@@ -0,0 +1,2 @@
+def area(radius: int) -> float:
+    return 3.141592653589793238462643383279502884197 * radius * radius
---
+++ backend/day-09/challengeday9.py
@@ -0,0 +1,4 @@
+from areaofcircle import area
+
+radius = int(input("Enter the radius of the circle: "))
+print(f"The area of the circle with radius {radius} is {area(radius):.2f}")
