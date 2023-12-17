274f5c12e751fb9949a8c775d5ba20e739f1bd91
7equinox <juliusevanestonilo08@gmail.com>
2023-12-17 18:17:25 +0800

    Backend (Day 11/30)

---
+++ backend/day-11/challengeday11.py
@@ -0,0 +1,24 @@
+def append_file(sort_name: list):
+    # with open("names.txt", "w") as file:
+    with open("C:\\Users\\JE\\Desktop\\Program\\AWSCC-CodeQuest-Backend\\backend\\day-11\\names.txt", "w") as file:
+        file.writelines(sort_name)
+
+
+def sort_list(name: list) -> list:
+    return sorted(name)
+
+
+def read_file() -> list:
+    # with open("names.txt", "r") as file:
+    with open("C:\\Users\\JE\\Desktop\\Program\\AWSCC-CodeQuest-Backend\\backend\\day-11\\names.txt", "r") as file:
+        name_list = file.readlines()
+    return name_list
+
+
+def main():
+    name_list = read_file()
+    name_list_sort = sort_list(name_list)
+    append_file(name_list_sort)
+
+
+main()
