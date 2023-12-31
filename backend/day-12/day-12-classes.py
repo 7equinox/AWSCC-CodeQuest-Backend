67f72b853dff073b87f43d3d9b3f349997e0a710
7equinox <juliusevanestonilo08@gmail.com>
2023-12-31 08:31:28 +0800

    Backend (Day 12/30)

--- backend/day-12/day-12-classes.py
+++ backend/day-12/day-12-classes.py
@@ -6,6 +6,10 @@ class Student:
         self.is_enrolled = is_enrolled
         self.classes = classes
         self.offenses = offenses
+        
+    # add a new class to the student
+    def add_class(self, new_class):
+        self.classes.append(new_class)
 
 student1 = Student("Juan Dela Cruz", 20, True, [], [])
 
@@ -15,3 +19,6 @@ print(f"Status: {'Enrolled' if student1.is_enrolled else 'Not Enrolled Yet'}")
 print(f"Classes: {student1.classes}")
 print(f"Offenses: {student1.offenses}")
 
+student1.add_class("Math")
+
+print(student1.classes)
